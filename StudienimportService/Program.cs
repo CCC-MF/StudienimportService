94722443342a314542719ad8fcc34db4618c14a4
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-05-16 08:47:55 +0200

    Add service installation commands for use within windows

--- StudienimportService/Program.cs
+++ StudienimportService/Program.cs
@@ -1,7 +1,45 @@
+using CliWrap;
 using StudienimportService;
 
+const string serviceName = "Studienimport Service";
+
+if (args is { Length: 1 })
+{
+    string executablePath =
+        Path.Combine(AppContext.BaseDirectory, "StudienimportService.exe");
+
+    if (args[0] is "/Install")
+    {
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "create", serviceName, $"binPath={executablePath}" })
+            .ExecuteAsync();
+    }
+    else if (args[0] is "/Uninstall")
+    {
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "stop", serviceName })
+            .ExecuteAsync();
+
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "delete", serviceName })
+            .ExecuteAsync();
+    }
+
+    return;
+}
+
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .UseWindowsService(options => { options.ServiceName = serviceName; })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<Worker>();
+
+        services.AddLogging(builder =>
+        {
+            builder.AddConfiguration(
+                context.Configuration.GetSection("Logging"));
+        });
+    })
     .Build();
 
 await host.RunAsync();
